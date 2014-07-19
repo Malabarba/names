aec03d5f66e2ce1ed79ca213e520f774e13238cc
Artur Malabarba <bruce.connor.am@gmail.com>
2014-07-19 10:35:15 +0100

    Less verbose tests by default

--- tests/tests.el
+++ tests/tests.el
@@ -3,5 +3,6 @@
   (add-to-list 'load-path (expand-file-name "../"))
   (add-to-list 'load-path (expand-file-name "./"))
   (require 'ert)
-  (setq debug-on-error t)
-  (setq byte-compile-debug t))
+  ;; (setq debug-on-error t)
+  ;; (setq byte-compile-debug t)
+  )
