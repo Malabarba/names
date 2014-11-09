9dbfdab518e577eb1a6c96e1ae225859891fafd1
Artur Malabarba <bruce.connor.am@gmail.com>
2014-11-09 18:39:23 +0000

    packages/names: Comment out a google-this test.
    It's failing for reasons unrelated to Names. Commenting it out while I
    figure it out.

--- names.el
+++ names.el
@@ -4,7 +4,7 @@
 
 ;; Author: Artur Malabarba <bruce.connor.am@gmail.com>
 ;; URL: http://github.com/Bruce-Connor/names
-;; Version: 0
+;; Version: 0.6
 ;; Package-Requires: ((emacs "24.1") (cl-lib "0.5"))
 ;; Keywords: extensions lisp
 ;; Prefix: names
