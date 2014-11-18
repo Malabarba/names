ed44aa9bcebcc055551a2d018db9080ee0e9866f
Artur Malabarba <bruce.connor.am@gmail.com>
2014-11-18 22:58:56 +0000

    packages/names: Version bump

--- names.el
+++ names.el
@@ -5,7 +5,7 @@
 ;; Author: Artur Malabarba <bruce.connor.am@gmail.com>
 ;; Maintainer: Artur Malabarba <bruce.connor.am@gmail.com>
 ;; URL: http://github.com/Bruce-Connor/names
-;; Version: 0
+;; Version: 20141119
 ;; Package-Requires: ((emacs "24.1") (cl-lib "0.5"))
 ;; Keywords: extensions lisp
 ;; Prefix: names
@@ -120,7 +120,7 @@ it will set PROP."
 
 ;;; ---------------------------------------------------------------
 ;;; Variables
-(defconst names-version "0.5.5" "Version of the names.el package.")
+(defconst names-version "20141119" "Version of the names.el package.")
 
 (defvar names--name nil
   "Name of the current namespace inside the `define-namespace' macro.")
